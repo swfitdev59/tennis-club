0e9cab9ab13176a5f02ae114e8a2e4c46370443c
swfitdev59 <swfitdev59@gmail.com>
2024-11-07 08:38:48 -0500

    initial repo for deploy

---
+++ members/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Member
+
+
+class MemberAdmin(admin.ModelAdmin):
+    list_display = ('firstname', 'lastname', 'phone', 'joined_date')
+    prepopulated_fields = {"slug": ("firstname", "lastname")}
+
+admin.site.register(Member, MemberAdmin)
